7fa3d53cfb7af1448be7bb149e54b797cc21bdca
anuragkurosaki <60319638+anuragkurosaki@users.noreply.github.com>
2020-05-16 22:08:21 +0530

    Changes

---
+++ app/src/main/java/com/anurag/androidbooking/AdminFinalViewActivity.java
@@ -0,0 +1,14 @@
+package com.anurag.androidbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminFinalViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_final_view);
+    }
+}

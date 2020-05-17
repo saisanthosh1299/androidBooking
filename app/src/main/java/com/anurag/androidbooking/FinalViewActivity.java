f1429ccc9f4a863887ee9f69c9aa6e98dba5f2c2
anuragkurosaki <60319638+anuragkurosaki@users.noreply.github.com>
2020-05-17 14:18:59 +0530

    logout method created

--- app/src/main/java/com/anurag/androidbooking/FinalViewActivity.java
+++ app/src/main/java/com/anurag/androidbooking/FinalViewActivity.java
@@ -3,12 +3,23 @@ package com.anurag.androidbooking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class FinalViewActivity extends AppCompatActivity {
+    Button logout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_view);
+
+        logout = findViewById(R.id.logOutButton);
+        logoutPressed();
+    }
+    public void logoutPressed() {
+        System.out.println("log OUt pressed");
     }
 }
+

602cd3e6d0cbf75ad05dd6a5f9422ec8bda622a7
anuragkurosaki <60319638+anuragkurosaki@users.noreply.github.com>
2020-05-31 23:26:08 +0530

    Update FinalViewActivity.java

--- app/src/main/java/com/anurag/androidbooking/FinalViewActivity.java
+++ app/src/main/java/com/anurag/androidbooking/FinalViewActivity.java
@@ -133,12 +133,12 @@ public class FinalViewActivity extends AppCompatActivity {
     }
 
     public void bookSlot(View view) {
-        //the button should already only be available when there is no booking for the user
-        //check that please // did you already write the code for that?
-        //see above but there are no bookings available here now i need to run the IOS app and book a slot will be doing it now
-        // it is working but taking time as it did in the ios app would be better if initially it was hidden and if there is no slot booked then the button may show up
-        // what do you think?
-        //right
+        //how many bookings are there at the momemnt
+        // 2
+
+        //2 current bookings ? So, set the max to 2, then the button should also NOT ber available
+        //ok
+        // done i guess
+        
     }
 }
-
